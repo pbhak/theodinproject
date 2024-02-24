3619bba90a38e3e36b3e198aef855ab5d7c41247
midnight0s <77811207+midnight0s@users.noreply.github.com>
2024-02-24 12:52:29 -0800

    Add substrings project

--- Ruby/Caesar/main.rb
+++ Ruby/Caesar/main.rb
@@ -14,30 +14,4 @@ def caesar_cipher(initial_string, shift)
   end.join('')
 end
 
-# def caesar_cipher(initial_string, shift)
-#   downcase_array = ('a'..'z').to_a
-#   upcase_array = ('A'..'Z').to_a
-
-#   string_array = initial_string.split('')
-
-#   string_array.map do |char|
-#     if !char.match(/[[:alpha:]]/)
-#       next
-#     elsif char == char.downcase
-#       if (downcase_array.index(char) + shift) <= 25
-#         string_array[string_array.index(char)] = downcase_array[downcase_array.index(char) + shift]
-#       else
-#         string_array[string_array.index(char)] = downcase_array[(downcase_array.index(char) + shift) - 26]
-#       end
-#     elsif char == char.upcase
-#       if (upcase_array.index(char) + shift) <= 25
-#         string_array[string_array.index(char)] = upcase_array[upcase_array.index(char) + shift]
-#       else
-#         string_array[string_array.index(char)] = upcase_array[(upcase_array.index(char) + shift) - 26]
-#       end
-#     end
-#   end
-#   string_array.join('')
-# end
-
 puts caesar_cipher("What a string!", 6)
---
+++ Ruby/Substrings/main.rb
@@ -0,0 +1,14 @@
+def substrings(word, dictionary)
+  word = word.downcase.split(' ')
+  dictionary.reduce(Hash.new(0)) do |result, value|
+    word.each_with_index do |word_value, i|
+      if word_value.include?(value)
+        result[value] += 1
+      end
+    end
+    result
+  end
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
