aa27453d4168fb011e36cb14e8eccfaaec9d68da
Neel32 <77811207+midnight0s@users.noreply.github.com>
2024-04-15 17:15:37 -0700

    finished work on feedback system

--- Ruby/Mastermind/game.rb
+++ Ruby/Mastermind/game.rb
@@ -2,10 +2,40 @@
 class Game
   def initialize
     @code = generate_code
+    @code_counts = Hash.new(0)
+    @code.each { |peg| @code_counts[peg] += 1 }
     p @code
   end
 
+  # private
+
   def generate_code
     [rand(1..6), rand(1..6), rand(1..6), rand(1..6)]
+  end
+
+  def feedback(guess)
+    guess_composition = {
+      exact: 0,
+      near: 0,
+      none: 0
+    }
+
+    guess_counts = Hash.new(0)
+    guess.each { |peg| guess_counts[peg] += 1 }
+
+    guess.each_with_index do |peg, index|
+      if @code[index] == peg
+        # Exact match
+        guess_composition[:exact] += 1
+      elsif @code_counts[peg].positive?
+        # Near match
+        guess_composition[:near] += 1
+      else
+        # No match
+        guess_composition[:none] += 1
+      end
+    end
+
+    guess_composition
   end
 end
--- Ruby/Mastermind/main.rb
+++ Ruby/Mastermind/main.rb
@@ -1,3 +1,11 @@
 require_relative 'game'
 
-_game = Game.new
+game = Game.new
+guess = []
+
+4.times do |i|
+  print "Guess #{i + 1}: "
+  guess << gets.chomp.to_i
+end
+
+p game.feedback(guess)
