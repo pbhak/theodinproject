3efc5e9ddec701baa5521c773ead71128eb092a5
Neel32 <77811207+midnight0s@users.noreply.github.com>
2024-05-06 17:57:18 -0700

    Last two sections remaining

--- Ruby/EventManager/lib/event_manager.rb
+++ Ruby/EventManager/lib/event_manager.rb
@@ -1,9 +1,86 @@
+require 'csv'
+require 'erb'
+require 'google/apis/civicinfo_v2'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def format_phone_number(phone_number)
+  phone_number_array = phone_number.split('')
+  final_number = ''
+
+  phone_number_array.each_with_index do |digit, index|
+    if index.zero?
+      final_number << "(#{digit}"
+    elsif index == 2
+      final_number << "#{digit}) "
+    elsif index == 5
+      final_number << "#{digit}-"
+    else
+      final_number << digit
+    end
+  end
+
+  final_number
+end
+
+def clean_phone_number(phone_number)
+  phone_number = phone_number.delete('^0-9')
+
+  if phone_number.length < 10 || phone_number.length > 11 || (phone_number.length == 11 && phone_number[0] != 1)
+    '(000) 000-0000'
+  elsif phone_number.length == 11 && phone_number[0] == 1
+    format_phone_number(phone_number[1..])
+  else
+    format_phone_number(phone_number)
+  end
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  begin
+    civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir 'output' unless Dir.exist? 'output'
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts 'Event Manager Initialized'
 
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-  next if index.zero?
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
 
-  columns = line.split(',')
-  puts columns[2]
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  phone_number = clean_phone_number(row[:homephone])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
 end
