8f85bc86cdc06479cbcde37336c210cb293f97d5
Neel32 <77811207+midnight0s@users.noreply.github.com>
2024-05-06 22:26:55 -0700

    Finish event manager

--- Ruby/EventManager/lib/event_manager.rb
+++ Ruby/EventManager/lib/event_manager.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'date'
 require 'erb'
 require 'google/apis/civicinfo_v2'
 
@@ -37,6 +38,16 @@ def clean_phone_number(phone_number)
   end
 end
 
+def convert_string_to_time(string_time)
+  date = string_time.split[0]
+  time = string_time.split[1]
+
+  date_split = date.split('/')
+  time_split = time.split(':')
+
+  Time.new(date_split[2].to_i + 2000, date_split[0], date_split[1], time_split[0], time_split[1])
+end
+
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -62,6 +73,7 @@ def save_thank_you_letter(id, form_letter)
 end
 
 puts 'Event Manager Initialized'
+puts
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -72,15 +84,50 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+registration_hours = Hash.new(0)
+peak_hours = []
+
+registration_days = Hash.new(0)
+peak_days = []
+
 contents.each do |row|
   id = row[0]
+  date_and_time = convert_string_to_time(row[:regdate])
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   phone_number = clean_phone_number(row[:homephone])
-
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
+  registration_hours[date_and_time.hour] += 1
+  registration_days[date_and_time.wday] += 1
+
   save_thank_you_letter(id, form_letter)
+end
+
+registration_hours.each do |hour, value|
+  peak_hours << hour if value == registration_hours.values.max
+end
+
+registration_days.each do |day, value|
+  peak_days << day if value == registration_days.values.max
+end
+
+puts 'Peak registration hours were: '
+
+peak_hours.each do |hour|
+  if hour > 12
+    puts "#{hour - 12} PM"
+  else
+    puts "#{hour} AM"
+  end
+end
+
+puts
+
+puts 'Peak registration days were: '
+
+peak_days.each do |day|
+  puts "#{Date::DAYNAMES[day]} - #{registration_days[day]} registrations"
 end
