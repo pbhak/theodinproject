2622d7abe44fb1b7fc7e091df03ec57006f2694b
Neel32 <77811207+midnight0s@users.noreply.github.com>
2024-10-02 17:21:01 -0700

    pretty much done

--- Ruby/merge_sort.rb
+++ Ruby/merge_sort.rb
@@ -29,6 +29,7 @@ end
 
 def merge_sort(array)
   array = split_array(array)
+  final_array = []
   
   array.each_with_index do |node, node_index|
     node.each_with_index do |subnode, subnode_index|
@@ -38,7 +39,60 @@ def merge_sort(array)
     end
   end
   
-  array
+  array.each_with_index do |node, node_index|
+    final_node = []
+    
+    4.times do
+      if node[0][0] < node[1][0]
+        final_node << node[0][0]
+        node[0].shift
+      elsif node[1][0] < node [0][0]
+        final_node << node[1][0]
+        node[1].shift
+      end
+      
+      break if node[0].empty? and node[1].empty?
+      if node[0].empty?
+        final_node << node[1][0]
+        break
+      elsif node[1].empty?
+        final_node << node[0][0]
+        break
+      end
+    end
+    
+    final_array << final_node
+  end
+  array = [final_array]
+  final_array = []
+  
+  # array is now [[[2, 17, 33, 77], [7, 9, 101]]]
+  array.each do |node|
+    final_node = []
+    loop do
+      if node[0][0] < node[1][0]
+        final_node << node[0][0]
+        node[0].shift
+      elsif node[1][0] < node [0][0]
+        final_node << node[1][0]
+        node[1].shift
+      end
+      
+      break if node[0].empty? and node[1].empty?
+      if node[0].empty?
+        final_node << node[1][0]
+        break
+      elsif node[1].empty?
+        final_node << node[0][0]
+        break
+      end
+    end
+    
+    final_array << final_node
+  end
+
+  final_array[0]
 end
 
 p merge_sort([17, 33, 2, 77, 7, 9, 103])
+
